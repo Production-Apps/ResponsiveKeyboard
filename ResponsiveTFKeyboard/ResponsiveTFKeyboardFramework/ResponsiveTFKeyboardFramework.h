6eaaf5995dc4153450ecfb9772121195ab563a11
fritzgt <tuanis@live.com>
2020-04-11 21:00:46 +0300

    added framework

---
+++ ResponsiveTFKeyboard/ResponsiveTFKeyboardFramework/ResponsiveTFKeyboardFramework.h
@@ -0,0 +1,19 @@
+//
+//  ResponsiveTFKeyboardFramework.h
+//  ResponsiveTFKeyboardFramework
+//
+//  Created by FGT MAC on 4/11/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResponsiveTFKeyboardFramework.
+FOUNDATION_EXPORT double ResponsiveTFKeyboardFrameworkVersionNumber;
+
+//! Project version string for ResponsiveTFKeyboardFramework.
+FOUNDATION_EXPORT const unsigned char ResponsiveTFKeyboardFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResponsiveTFKeyboardFramework/PublicHeader.h>
+
+
